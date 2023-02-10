94bcb5148b656ffb2009990643a37727e2157841
Aurélien Lansmanne <aurelien.lansmanne@gmail.com>
2023-02-10 18:54:50 +0100

    PWA manifest and service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {FormsModule} from "@angular/forms";
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {environment} from '../environments/environment';
 
 // AoT requires an exported function for factories
 export function createTranslateLoader(http: HttpClient) {
@@ -32,7 +34,13 @@ export function createTranslateLoader(http: HttpClient) {
     IonicModule.forRoot(),
     AppRoutingModule,
     IonicModule.forRoot(),
-    FormsModule
+    FormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [
     {provide: RouteReuseStrategy, useClass: IonicRouteStrategy}
