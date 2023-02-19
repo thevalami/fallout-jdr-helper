ee833bffbeaeb77c7c23d53928f785592a006eb4
Aurélien Lansmanne <aurelien.lansmanne@gmail.com>
2023-02-19 16:14:46 +0100

    Oops :)

--- src/app/data/books/book-lang.ts
+++ src/app/data/books/book-lang.ts
@@ -1,6 +1,5 @@
 import {BOOK_TABLE_INDEX_FR} from "./fr/books-table-index";
 import {BOOK_TABLE_INDEX_EN} from "./en/books-table-index";
-import {BOOK_TABLE_INDEX_IT} from "./it/books-table-index";
 
 export interface BookType {
   "Dice": number,
@@ -18,5 +17,4 @@ export interface BookVolume {
 export const BOOK_TABLE_INDEX = {
   'fr': BOOK_TABLE_INDEX_FR,
   'en': BOOK_TABLE_INDEX_EN
-  'it': BOOK_TABLE_INDEX_IT
 }
